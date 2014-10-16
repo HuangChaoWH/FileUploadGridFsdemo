4fe24cc53851d6e975e6cb778f2a8640802efd25
gautam anand <gautamanand@124-99.priv23.nus.edu.sg>
2014-10-16 16:43:21 +0800

    Added GET to fetch list of Uploaded files in directory

--- app.js
+++ app.js
@@ -2,31 +2,61 @@ var express = require('express');    //Express Web Server
 var busboy = require('connect-busboy'); //middleware for form/file upload
 var path = require('path');     //used for file path
 var fs = require('fs-extra');       //File System - for file manipulation
+var fsold = require('fs');
+
 
 var app = express();
 app.use(busboy());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.route('/upload')
-    .post(function (req, res, next) {
 
-        //Function to upload the file and pass it to the folder
+//Get a list of all the files in the directory
 
+app.get('/upload', function(req, res){
+
+        fs.readdir( __dirname + '/public/file/', function(err, files) {
+
+            if (err) {
+
+                res.send(err);
+            }
+            res.json(files); // return all files in JSON format
+        });
+ 
+});
+
+
+//Upload a file to the dirrectory
+
+app.post('/upload', function (req, res, next) {
         var fstream;
-        req.pipe(req.busboy);
-        req.busboy.on('file', function (fieldname, file, filename) {
-            console.log("Uploading: " + filename);
 
-            //Path where image will be uploaded
+
+        req.pipe(req.busboy);
+
+        req.busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+
+            console.log ("filename: " + filename);
+ 
+         //Changing filename to the text entered
+
+            console.log ("fieldname: " + fieldname);
+            console.log ("encoding: " + encoding);
+            console.log ("mimetype: " + mimetype);
+
+            //Path where file will be uploaded with original filename
             fstream = fs.createWriteStream(__dirname + '/public/file/' + filename);
             file.pipe(fstream);
+
             fstream.on('close', function () {    
                 console.log("Upload Finished of " + filename);              
                 res.redirect('back');           //where to go next
             });
         });
+
+
     });
 
-var server = app.listen(3030, function() {
+var server = app.listen(3031, function() {
     console.log('Listening on port %d', server.address().port);
 });
